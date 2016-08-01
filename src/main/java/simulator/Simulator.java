9495429deddafbbe1377247dbff3076ba76cb249
JunoJunho <wnsgh611@gmail.com>
2016-08-01 22:19:51 +0900

    Simulation Procedure #1  - Outer loop with its outer condition implemented.

--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -4,4 +4,20 @@ package simulator;
  * Created by Junho on 2016-08-01.
  */
 public class Simulator {
+
+    public void execute(){
+        this.procedure();
+    }
+
+    /*
+    Simulation Procedure function from "Simulation and SMC of Logic-Based MAS Models"
+     */
+    private void procedure(){
+        boolean verdict = false;
+        boolean endCondition = false;
+        while(!endCondition){
+            System.out.println("Hello Simulator :)");
+            endCondition = true;
+        }
+    }
 }
