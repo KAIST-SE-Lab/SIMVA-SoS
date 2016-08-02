449b479e41df09d48c710d6ffd5e4fb95e08c402
JunoJunho <wnsgh611@gmail.com>
2016-08-02 21:41:57 +0900

    Simple Scenario is done.  - 2 CS, 2 Action, no rest, no SoS manager and its acknowledgement  - within 100 ticks

--- src/main/java/simulator/Action.java
+++ src/main/java/simulator/Action.java
@@ -2,18 +2,23 @@ package simulator;
 
 public class Action {
 
+    private String name;
     private int cost;
     private int benefit;
     private int SoSBenefit;
     private boolean isImmediate;
     private Constituent performer;
 
-    public Action(int cost, int benefit, int SoSBenefit, Constituent performer){
+    public Action(String name, int cost, int benefit, int SoSBenefit){
+        this.name = name;
         this.cost = cost;
         this.benefit = benefit;
         this.SoSBenefit = SoSBenefit;
-        this.performer = performer;
         this.isImmediate = true;
+    }
+
+    public void updatePerformer(Constituent performer){
+        this.performer = performer;
     }
 
     public int getCost(){
@@ -34,5 +39,9 @@ public class Action {
 
     public boolean isImmediate(){
         return this.isImmediate;
+    }
+
+    public String toString(){
+        return this.name;
     }
 }
---
+++ src/main/java/simulator/SoSManager.java
@@ -0,0 +1,15 @@
+package simulator;
+
+public class SoSManager extends Constituent {
+
+    private int SoSBenefit;
+
+    public SoSManager(){
+        super("SoS manager");
+        this.SoSBenefit = 0;
+    }
+
+    public void ack(){
+
+    }
+}
