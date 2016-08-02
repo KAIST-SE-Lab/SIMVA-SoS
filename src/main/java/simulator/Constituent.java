e88b5017996a28c50a0c62a07bda5f40bcf09e37
JunoJunho <wnsgh611@gmail.com>
2016-08-02 20:00:03 +0900

    Temporal Constituent updated

--- src/main/java/simulator/Constituent.java
+++ src/main/java/simulator/Constituent.java
@@ -16,17 +16,52 @@ public class Constituent {
         this.cumulatedBenefit = 0;
     }
 
+    /**
+     * step method
+     * CS chooses an action according to probability distribution.
+     * Before selecting, the CS checks the acknowledgement from SoS manager.
+     * @return chosen Action instance
+     */
     public Action step(){
-        return new Action(0, 0, 0, this);
+        if(this.getRemainBudget() == 0){
+            return new Action(0, 0, 0, this); // voidAction, nothing happen
+        }else{
+            // Need to updated, currently select best utility action
+            ArrayList<Action> tempList = new ArrayList<Action>(this.actionList.size());
+            tempList.addAll(this.actionList);
+            for(Action a : tempList){
+                if(a.getCost() > this.getRemainBudget()){
+                    tempList.remove(a);
+                }
+            }
+            if(tempList.size() > 0)
+                return selectAction(tempList);
+            else
+                return new Action(0, 0, 0, this);
+        }
     }
 
-    public int getRemainCost(){
+    public Action selectAction(ArrayList<Action> actions){
+        Action retAction = actions.get(0);
+        for(Action a : actions){
+            if(getUtility(a) > getUtility(retAction)){
+                retAction = a;
+            }
+        }
+        return retAction;
+    }
+
+    public int getRemainBudget(){
         return this.totalBudget - this.usedCost;
     }
 
     public void updateCostBenefit(int cost, int benefit){
         this.usedCost += cost;
         this.cumulatedBenefit += benefit;
+    }
+
+    public int getUtility(Action a){
+        return a.getBenefit() - a.getCost();
     }
 
 }
