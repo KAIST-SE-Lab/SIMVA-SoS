e9dd5c0bb77a98994235894667b550611e4f8269
JunoJunho <wnsgh611@gmail.com>
2016-08-20 13:55:59 +0900

    Simulator update - CS has capability - Each CS shares common actions, but they have different cost to execute actions. - Environment frame code has been implemented.

--- src/main/java/simulator/Constituent.java
+++ src/main/java/simulator/Constituent.java
@@ -1,10 +1,14 @@
 package simulator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Constituent {
 
     private ArrayList<Action> actionList = null;
+    private ArrayList<Action> capabilityList = null; // 일단 보존
+    private HashMap<String, Integer> capabilityMap = null; // 각 CS의 Action 당 사용되는 cost
+
     private String name;
     private int usedCost;
     private int totalBudget;
@@ -13,6 +17,8 @@ public class Constituent {
     public Constituent(String name){
         this.name = name;
         this.actionList = new ArrayList<Action>();
+        this.capabilityList = new ArrayList<Action>();
+        this.capabilityMap = new HashMap<String, Integer>();
         this.usedCost = 0;
         this.totalBudget = 100;
         this.accumulatedBenefit = 0;
@@ -26,28 +32,24 @@ public class Constituent {
      */
     public Action step(){
         if(this.getRemainBudget() == 0){
-            Action a = new Action("void", 0, 0, 0);
-            a.updatePerformer(this);
-            return a; // voidAction, nothing happen
-        }else{
+            return null; // voidAction, nothing happen
+        }else{ // We have money
             ArrayList<Action> tempList = new ArrayList<Action>(this.actionList.size());
             tempList.addAll(this.actionList);
             for(Action a : this.actionList){
-                if(a.getCost() > this.getRemainBudget()){
+                if(this.getCost(a) > this.getRemainBudget()){
                     tempList.remove(a);
                 }
             }
             if(tempList.size() > 0)
                 return selectAction(tempList);
-            else{
-                Action a = new Action("void", 0, 0, 0);
-                a.updatePerformer(this);
-                return a;
+            else{ // We don't have enough money to execute an action.
+                return null;
             }
         }
     }
 
-    public Action selectAction(ArrayList<Action> actions){
+    private Action selectAction(ArrayList<Action> actions){
         // TODO: 2016-08-02 Select action based on probability distribution of utility
         Action retAction = actions.get(0);
         for(Action a : actions){
@@ -67,8 +69,8 @@ public class Constituent {
         this.accumulatedBenefit += benefit;
     }
 
-    public int getUtility(Action a){
-        return a.getBenefit() - a.getCost();
+    private int getUtility(Action a){
+        return a.getBenefit() - this.getCost(a);
     }
 
     public String toString(){
@@ -79,9 +81,25 @@ public class Constituent {
         return this.accumulatedBenefit;
     }
 
-    public void addAction(Action a){
-        this.actionList.add(a);
-        a.updatePerformer(this);
+    public void addCapability(Action a, int cost){
+        this.capabilityList.add(a);
+        this.capabilityMap.put(a.getName(), cost);
+    }
+
+    public ArrayList<Action> getCapability(){
+        return this.capabilityList;
+    }
+
+    public ArrayList<Action> getActionList(){
+        return this.actionList;
+    }
+
+    public int getCost(Action a){
+        Integer _cost = this.capabilityMap.get(a.getName());
+        if(_cost != null)
+            return _cost;
+        else
+            return 0;
     }
 
 }
