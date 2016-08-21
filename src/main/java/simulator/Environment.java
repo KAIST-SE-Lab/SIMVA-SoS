ce203d298ff9f4134b81e063b4e2238e338ffe54
JunoJunho <wnsgh611@gmail.com>
2016-08-21 22:05:00 +0900

    Environment randomly generate the actions

--- src/main/java/simulator/Environment.java
+++ src/main/java/simulator/Environment.java
@@ -1,8 +1,6 @@
 package simulator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Simulator for System of Systems
@@ -18,8 +16,8 @@ public class Environment {
      * 2. 현재 각 Action 의 진행 상황
      */
 
-    private ArrayList<Constituent> csList = null;
-    private ArrayList<Action> actionList = null;
+    private ArrayList<Constituent> csList = null; // 모든 CS list
+    private ArrayList<Action> actionList = null; // 모든 Action List
     private HashMap<String, Action.Status> statusHashMap = null;
 
     public Environment(Constituent[] CSs, Action[] actions){
@@ -42,7 +40,34 @@ public class Environment {
      * If the number of actions to be raised is 0, then no action is raised.
      */
     public void generateAction(){
-        int numPossibleActions = 0;
+        /**
+         * 1. Count the number of possible actions to be raised
+         * 2. Generate a random number that how many actions will be raised
+         * 3. Shuffle the possible action list and pick actions
+         */
+        ArrayList<String> possibleActionList = new ArrayList<String>();
+        for(Map.Entry<String, Action.Status> entry: this.statusHashMap.entrySet()){
+            if(entry.getValue() == Action.Status.Not_raised){
+                possibleActionList.add(entry.getKey());
+            }
+        }
+
+        Random randomGenerator = new Random();
+        int numRaisingActions = randomGenerator.nextInt(possibleActionList.size()+1);
+        ArrayList<Action> selectedActionList = new ArrayList<Action>();
+        Collections.shuffle(possibleActionList);
+        for(int i = 0; i < numRaisingActions; i--){
+            String actionName = possibleActionList.get(i);
+            for(Action a : this.actionList){
+                if(a.getName().equalsIgnoreCase(actionName)){
+                    a.setStatus(Action.Status.Raised);
+                    selectedActionList.add(a);
+                    break;
+                }
+            }
+        }
+
+        updateActionStatus(selectedActionList);
     }
 
     /**
@@ -56,7 +81,7 @@ public class Environment {
      * Update the status of actions executed by CSs
      * @param actionList the list of actions that are changed its status
      */
-    public void updateActionStatus(ArrayList<Action> actionList){
+    private void updateActionStatus(ArrayList<Action> actionList){
         for(Action a : actionList){
             Action.Status newStatus = a.getStatus();
             this.statusHashMap.remove(a.getName());
