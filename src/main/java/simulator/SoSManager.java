c2e2a94aba028f3bf1065c73b69e392f7370ce65
JunoJunho <wnsgh611@gmail.com>
2016-08-25 08:39:22 +0900

    SoS Manager default frame

--- src/main/java/simulator/SoSManager.java
+++ src/main/java/simulator/SoSManager.java
@@ -1,15 +1,21 @@
 package simulator;
 
+import java.util.ArrayList;
+
 public class SoSManager extends Constituent {
 
     private int SoSBenefit;
+    private ArrayList<Constituent> csList;
 
-    public SoSManager(){
-        super("SoS manager");
+
+    public SoSManager(String name, ArrayList<Constituent> csList) {
+        super(name);
+        this.csList = csList;
         this.SoSBenefit = 0;
     }
 
+
     public void ack(){
-        // // TODO: 2016-08-02 Implement SoS manager acknowledgement
+        // TODO: 2016-08-02 Implement SoS manager acknowledgement
     }
 }
