d54e9025f64f673698620b7d41b71819bc33b0c5
JunoJunho <wnsgh611@gmail.com>
2016-08-29 00:01:57 +0900

    Example update

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,12 +12,15 @@ public class Main {
         Constituent cs2 = new Constituent("CS2");
         Action a1 = new Action("Action1", 2, 1, 2);
         Action a2 = new Action("Action2", 4, 2, 3);
+        Action a3 = new Action("Action3", 3, 5, 4);
         cs1.addCapability(a1, 1);
         cs1.addCapability(a2, 2);
+        cs1.addCapability(a3, 3);
         cs2.addCapability(a1, 1);
+        cs2.addCapability(a3, 4);
 
         Constituent[] CSs = {cs1, cs2};
-        Action[] actions = {a1, a2};
+        Action[] actions = {a1, a2, a3};
         SoSManager manager = new SoSManager("SoS Manager", CSs, actions);
         Environment env = new Environment(CSs, actions);
 
