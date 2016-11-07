3381916e6275b33324e9cee537dc39dbe7e983d3
JunoJunho <wnsgh611@gmail.com>
2016-11-07 17:37:05 +0900

    Remove logger info from the large-scale

--- src/main/java/mci/LargeScaleMCIMain.java
+++ src/main/java/mci/LargeScaleMCIMain.java
@@ -4,8 +4,6 @@ import com.opencsv.CSVWriter;
 import kr.ac.kaist.se.mc.BaseChecker;
 import kr.ac.kaist.se.simulator.*;
 import kr.ac.kaist.se.simulator.method.SPRTMethod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
@@ -29,11 +27,10 @@ import java.util.Date;
  * furnished to do so, subject to the following conditions: TBD
  */
 public class LargeScaleMCIMain {
-    public static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         NormalDistributor distributor = new NormalDistributor();
-        distributor.setNormalDistParams(8000, 2500);
+        distributor.setNormalDistParams(2500, 800);
 
         NearestPTS np1 = new NearestPTS();
         NearestPTS np2 = new NearestPTS();
@@ -50,6 +47,11 @@ public class LargeScaleMCIMain {
         NearestPTS np13 = new NearestPTS();
         NearestPTS np14 = new NearestPTS();
         NearestPTS np15 = new NearestPTS();
+        NearestPTS np16 = new NearestPTS();
+        NearestPTS np17 = new NearestPTS();
+        NearestPTS np18 = new NearestPTS();
+        NearestPTS np19 = new NearestPTS();
+        NearestPTS np20 = new NearestPTS();
 
 
         SeverityPTS sp1 = new SeverityPTS();
@@ -67,10 +69,15 @@ public class LargeScaleMCIMain {
         SeverityPTS sp13 = new SeverityPTS();
         SeverityPTS sp14 = new SeverityPTS();
         SeverityPTS sp15 = new SeverityPTS();
+        SeverityPTS sp16 = new SeverityPTS();
+        SeverityPTS sp17 = new SeverityPTS();
+        SeverityPTS sp18 = new SeverityPTS();
+        SeverityPTS sp19 = new SeverityPTS();
+        SeverityPTS sp20 = new SeverityPTS();
 
         BaseConstituent[] CSs = new BaseConstituent[]{np1, np2, np3, np4, np5, np6, np7, np8, np9, np10,
-                np11, np12, np13, np14, np15, sp11, sp12, sp13, sp14, sp15,
-                sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10};
+                np11, np12, np13, np14, np15, sp11, sp12, sp13, sp14, sp15, np16, np17, np18, np19, np20,
+                sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10, sp16, sp17, sp18, sp19, sp20};
 
         Hospital hos = new Hospital();
         ArrayList<RescueAction> rActions = new ArrayList<>();
@@ -86,94 +93,77 @@ public class LargeScaleMCIMain {
             SimpleDateFormat transFormat = new SimpleDateFormat("yyyyMMddHHmmss");
             String pre = transFormat.format(nowDate);
 
-            for (int trial = 1; trial <= 30; trial++) {
+            String outputName = "mci_result/" + pre + "_Large_MCI_" + bound + "_" + String.format("%.3f", alpha_beta) + ".csv";
+            CSVWriter cw = new CSVWriter(new OutputStreamWriter(new FileOutputStream(outputName), "UTF-8"), ',', '"');
+            cw.writeNext(new String[]{"prob", "num_of_samples", "execution_time", "result"});
+            ArrayList<SMCResult> resList = new ArrayList<>();
 
-                String outputName = "mci_result/" + pre + "_Large_MCI_" + bound + "_" + String.format("%.3f", alpha_beta) + "trial_"+ String.valueOf(trial) +".csv";
-                CSVWriter cw = new CSVWriter(new OutputStreamWriter(new FileOutputStream(outputName), "UTF-8"), ',', '"');
-                cw.writeNext(new String[]{"prob", "num_of_samples", "execution_time", "result"});
-                ArrayList<SMCResult> resList = new ArrayList<>();
+            System.out.println("----------------------------------------------------");
+            System.out.println("SoS-level benefit is greater than and equal to " + bound + ".");
+            BaseChecker checker = new BaseChecker(9000, bound, BaseChecker.comparisonType.GREATER_THAN_AND_EQUAL_TO);
+            SPRTMethod sprt = new SPRTMethod(alpha_beta, alpha_beta, 0.01); // 신뢰도 99%
 
-                System.out.println("----------------------------------------------------");
-                System.out.println("SoS-level benefit is greater than and equal to " + bound + ".");
-                BaseChecker checker = new BaseChecker(20000, bound, BaseChecker.comparisonType.GREATER_THAN_AND_EQUAL_TO);
-                SPRTMethod sprt = new SPRTMethod(alpha_beta, alpha_beta, 0.01); // 신뢰도 99%
+            for (int t = 1; t < 100; t++) {
+                double theta = 0.01 * t; // theta
 
-                for (int t = 1; t < 100; t++) {
-                    double theta = 0.01 * t; // theta
+                long start = System.currentTimeMillis();
+                sprt.setExpression(theta);
 
-                    long start = System.currentTimeMillis();
-                    sprt.setExpression(theta);
+                while (!sprt.checkStopCondition()) {
 
-                    logger.info("------------------------------------------- Theta : "
-                            + String.format("%.2f", theta) + " ---------------------------------------------------");
-                    long local_start = System.currentTimeMillis();
-
-                    while (!sprt.checkStopCondition()) {
-                        logger.info("[Large] CheckStopCondition :" + String.format("%.4f", (System.currentTimeMillis() - local_start) / 1000.0));
-
-                        // Initialize Patient map
-                        for (int i = 0; i <= 100; i++) {
-                            Hospital.GeoMap.add(new MapPoint(i));
-                        }
-
-                        ArrayList<Integer> list = new ArrayList<>();
-                        list.clear();
-                        list = distributor.getDistributionArray(500); // Action 수 만큼 증가
-
-                        sim.setActionPlan(list);
-                        sim.setEndTick(20000);
-
-                        logger.info("Initialize :" + String.format("%.4f", (System.currentTimeMillis() - local_start) / 1000.0));
-
-                        sim.execute();
-
-                        logger.info("Simulator Execution :" + String.format("%.4f", (System.currentTimeMillis() - local_start) / 1000.0));
-
-                        SIMResult res = sim.getResult();
-                        int checkResult = checker.evaluateSample(res);
-                        sprt.updateResult(checkResult);
-
-                        logger.info("The result is " + String.valueOf(res.getSoSBenefit()));
-
-                        System.gc();
-
-                        sim.reset();
-                        sim.setActionPlan(list);
-
-                        logger.info("SPRT Calculation :" + String.format("%.2f", (System.currentTimeMillis() - local_start) / 1000.0));
-
-                        local_start = System.currentTimeMillis();
+                    // Initialize Patient map
+                    for (int i = 0; i <= 100; i++) {
+                        Hospital.GeoMap.add(new MapPoint(i));
                     }
 
-                    boolean h0 = sprt.getResult(); // Result
-                    int numSamples = sprt.getNumSamples();
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.clear();
+                    list = distributor.getDistributionArray(500); // Action 수 만큼 증가
 
-                    long exec_time = System.currentTimeMillis() - start; //exec time
-                    int minTick = checker.getMinTick();
-                    int maxTick = checker.getMaxTick();
-                    sprt.reset();
-                    resList.add(new SMCResult(theta, numSamples, exec_time, minTick, maxTick, h0));
-                    System.out.print("Theta: " + theta);
-                    if (h0) {
-                        System.out.print(" Result: T");
-                    } else {
-                        System.out.print(" Result: F");
-                    }
-                    System.out.print(" with n of samples: " + numSamples);
-                    System.out.println(" in " + String.format("%.2f", exec_time / 1000.0) + " secs");
+                    sim.setActionPlan(list);
+                    sim.setEndTick(9000);
+
+                    sim.execute();
+
+                    SIMResult res = sim.getResult();
+                    int checkResult = checker.evaluateSample(res);
+                    sprt.updateResult(checkResult);
+
+                    System.gc();
+
+                    sim.reset();
+                    sim.setActionPlan(list);
 
                 }
-                System.out.println();
-                for (SMCResult r : resList) {
-                    System.out.print(".");
-                    cw.writeNext(r.getArr());
-                }
-                cw.close();
-                resList.clear();
-                System.out.println();
 
-                System.out.println("Finished");
+                boolean h0 = sprt.getResult(); // Result
+                int numSamples = sprt.getNumSamples();
+
+                long exec_time = System.currentTimeMillis() - start; //exec time
+                int minTick = checker.getMinTick();
+                int maxTick = checker.getMaxTick();
+                sprt.reset();
+                resList.add(new SMCResult(theta, numSamples, exec_time, minTick, maxTick, h0));
+                System.out.print("Theta: " + theta);
+                if (h0) {
+                    System.out.print(" Result: T");
+                } else {
+                    System.out.print(" Result: F");
+                }
+                System.out.print(" with n of samples: " + numSamples);
+                System.out.println(" in " + String.format("%.2f", exec_time / 1000.0) + " secs");
+
             }
+            System.out.println();
+            for (SMCResult r : resList) {
+                System.out.print(".");
+                cw.writeNext(r.getArr());
+            }
+            cw.close();
+            resList.clear();
+            System.out.println();
+
+            System.out.println("Finished");
         }
     }
 }
