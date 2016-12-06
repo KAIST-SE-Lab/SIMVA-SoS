9ec1abfda7f9a7aa9da2b918bd620b7088a3316a
JunoJunho <wnsgh611@gmail.com>
2016-12-06 21:38:09 +0900

    Add comment for AbsenceChecker

--- src/main/java/mci/checker/AbsenceChecker.java
+++ src/main/java/mci/checker/AbsenceChecker.java
@@ -29,9 +29,11 @@ public class AbsenceChecker implements CheckerInterface{
 
     /**
      * evaluateSample Method
-     *
-     * @param res
-     * @return
+     * Evaluate a given property satisfies absence property
+     * Check all time ticks whether there is a patient whose status changes
+     * from DEAD to other status (Dangerous or Very_Dangerous)
+     * @param res Simulation result class which contains debugTick Map
+     * @return 1, there is an absence, otherwise 0
      */
     @Override
     public int evaluateSample(SIMResult res) {
@@ -46,14 +48,14 @@ public class AbsenceChecker implements CheckerInterface{
                     if(patientStatusMap.containsKey(name)){
                         String beforeStat = patientStatusMap.get(name);
                         if(beforeStat.equalsIgnoreCase("DEAD") && !stat.equalsIgnoreCase("DEAD"))
-                            return 0;
+                            return 1;
                     }else{
                         patientStatusMap.put(name, stat);
                     }
                 }
             }
         }
-        return 1;
+        return 0;
     }
 
     @Override
