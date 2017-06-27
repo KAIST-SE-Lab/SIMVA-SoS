c44c28f796bef24dc8ffcd53c00374ea8d832018
claudejin <mgjin@se.kaist.ac.kr>
2017-06-27 15:25:37 +0900

    Link to the old version of SIMSoS

--- src/main/java/simsos/SIMSoS.java
+++ src/main/java/simsos/SIMSoS.java
@@ -1,5 +1,6 @@
 package simsos;
 
+import mci.Main;
 import simsos.propcheck.PropertyChecker;
 import simsos.sa.method.SPRT;
 import simsos.scenario.robot.RobotScenario;
@@ -8,13 +9,27 @@ import simsos.simulation.component.Scenario;
 import simsos.simulation.component.Snapshot;
 import simsos.simulation.component.World;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by mgjin on 2017-06-12.
  */
 public class SIMSoS {
     public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("old")) {
+            String[] passedArgs = Arrays.copyOfRange(args, 1, args.length);
+            try {
+                Main.experimentMain(passedArgs);
+            } catch (IOException e) {
+                System.out.println("Error: Old version is not runnable");
+                e.printStackTrace();
+            } finally {
+                System.exit(0);
+            }
+        }
+
         Scenario scenario = new RobotScenario(3);
         World world = scenario.getWorld();
         PropertyChecker checker = scenario.getChecker();
