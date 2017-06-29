e0902a0435766f91d66a7a099e56fe2df82b9ed3
claudejin <mgjin@se.kaist.ac.kr>
2017-06-29 12:08:13 +0900

    Implement PTS actions

--- src/main/java/simsos/scenario/mci/PTS.java
+++ src/main/java/simsos/scenario/mci/PTS.java
@@ -5,12 +5,17 @@ import simsos.simulation.component.Agent;
 import simsos.simulation.component.World;
 
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by mgjin on 2017-06-28.
  */
 public class PTS extends Agent {
+    private enum Status {Waiting, OnRescue}
+
     private String name;
+    private Status status;
+    private Location location;
 
     public PTS(World world, String name) {
         super(world);
@@ -21,12 +26,18 @@ public class PTS extends Agent {
 
     @Override
     public Action step() {
-        return null;
+        if (this.status == Status.Waiting)
+            return Action.getNullAction(1, "Waiting");
+        else // if (this.status == Status.OnRescue)
+            return Action.getNullAction(1, "Move");
     }
 
     @Override
     public void reset() {
+        Random rd = new Random();
 
+        this.status = Status.Waiting;
+        this.location = new Location(rd.nextInt(9), rd.nextInt(9));
     }
 
     @Override
@@ -36,6 +47,6 @@ public class PTS extends Agent {
 
     @Override
     public HashMap<String, Object> getProperties() {
-        return null;
+        return new HashMap<String, Object>();
     }
 }
