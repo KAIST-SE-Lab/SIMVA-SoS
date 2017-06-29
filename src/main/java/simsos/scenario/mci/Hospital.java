e42537d9f4b976bf1edc1c336aa0327860e04ada
claudejin <mgjin@se.kaist.ac.kr>
2017-06-29 11:21:34 +0900

    Implement Hospital actions

--- src/main/java/simsos/scenario/mci/Hospital.java
+++ src/main/java/simsos/scenario/mci/Hospital.java
@@ -4,13 +4,21 @@ import simsos.simulation.component.Action;
 import simsos.simulation.component.Agent;
 import simsos.simulation.component.World;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by mgjin on 2017-06-28.
  */
 public class Hospital extends Agent {
     private String name;
+    private Location location;
+
+    private int capacity;
+    private ArrayList<Patient> inpatients;
+
+    private Action treatment;
 
     public Hospital(World world, String name) {
         super(world);
@@ -21,12 +29,35 @@ public class Hospital extends Agent {
 
     @Override
     public Action step() {
-        return null;
+        if (this.inpatients.size() > 0)
+            return this.treatment;
+        else
+            return Action.getNullAction(1, "No treatment");
     }
 
     @Override
     public void reset() {
+        Random rd = new Random();
 
+        this.location = new Location(rd.nextInt(9), rd.nextInt(9));
+
+        this.capacity = 30 + (rd.nextInt(20) - 10); // 30 +- 10
+        this.inpatients = new ArrayList<Patient>();
+
+        this.treatment = new Action(1) {
+
+            @Override
+            public void execute() {
+                for (Patient patient: inpatients) {
+                    //give a treatment message to patient
+                }
+            }
+
+            @Override
+            public String getName() {
+                return "Treatment";
+            }
+        };
     }
 
     @Override
@@ -36,6 +67,6 @@ public class Hospital extends Agent {
 
     @Override
     public HashMap<String, Object> getProperties() {
-        return null;
+        return new HashMap<String, Object>();
     }
 }
