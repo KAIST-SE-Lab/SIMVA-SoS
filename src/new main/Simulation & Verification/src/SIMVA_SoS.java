53f6cd5023a0b93642e4a4492210dfdbec80ca47
sumin0407 <smpark@se.kaist.ac.kr>
2018-05-10 11:24:14 +0900

    test account

--- src/new main/Simulation & Verification/src/SIMVA_SoS.java
+++ src/new main/Simulation & Verification/src/SIMVA_SoS.java
@@ -1,5 +1,6 @@
 public class SIMVA_SoS {
     public static void main (String[] args) {
         System.out.println("Test for haha");
+        System.out.println("Test for account! psumin? sumin0407?");
     }
 }
