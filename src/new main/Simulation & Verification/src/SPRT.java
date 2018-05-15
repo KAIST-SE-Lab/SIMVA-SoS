e7e21706ce6110adb075cecd817729b54eb76f40
abalon1210 <abalon1210@daum.net>
2018-05-15 18:21:29 +0900

    Property Implementation
    Except SPRT.java. other property related java file implementation added
     ex) Property.java, PropertyChecker.java, ...
    
    Based on Prototype

--- src/new main/Simulation & Verification/src/SPRT.java
+++ src/new main/Simulation & Verification/src/SPRT.java
@@ -3,13 +3,13 @@ public class SPRT extends Verifier {
   double beta;
   double delta;
   int minimumSample;
+  
   public SPRT(PropertyChecker checker) {
     super(checker);
     this.alpha = 0.05;
     this.beta = 0.05;
     this.delta = 0.01;
     this.minimumSample = 2;
-
   }
 
 }
