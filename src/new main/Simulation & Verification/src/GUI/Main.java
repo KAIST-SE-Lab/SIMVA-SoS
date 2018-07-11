68a5816265a41e7c95ffb5475e18d64442080eea
Taegeon Lee <dlxorjs663@gmail.com>
2018-07-11 18:00:42 +0900

    Test GUI branch (new branch)

--- src/new main/Simulation & Verification/src/GUI/Main.java
+++ src/new main/Simulation & Verification/src/GUI/Main.java
@@ -2,7 +2,8 @@ package GUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Test GUI branch");
+
 
     }
 }
