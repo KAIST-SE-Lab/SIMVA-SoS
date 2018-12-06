6147b25f0281f348933ca2eec6080d991c393ae3
zele <zelalem@se.kaist.ac.kr>
2018-12-06 23:15:10 +0900

    Update SPRTTest.java

--- src/Junit_test/verifier/SPRTTest.java
+++ src/Junit_test/verifier/SPRTTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SPRTTest extends Verifier {
-
+    // SPRT test 
     public SPRTTest(PropertyChecker checker) {
         super(checker);
     }
