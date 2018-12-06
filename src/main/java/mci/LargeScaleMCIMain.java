96dd6738d55780db8fb7656fda43bf916408b709
zele <zelalem@se.kaist.ac.kr>
2018-12-06 23:12:22 +0900

    Update LargeScaleMCIMain.java

--- src/main/java/mci/LargeScaleMCIMain.java
+++ src/main/java/mci/LargeScaleMCIMain.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
  * furnished to do so, subject to the following conditions: TBD
  */
 public class LargeScaleMCIMain {
+       // MCI case referenced
 
     public static void main(String[] args) throws Exception {
 
