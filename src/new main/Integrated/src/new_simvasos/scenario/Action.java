dbb0f658f1caf63d6682ca7cee3c89a26a7c65dc
YONGJUN SHIN <yongjun9461@gmail.com>
2018-12-09 04:06:36 +0900

    action javadoc

--- src/new main/Integrated/src/new_simvasos/scenario/Action.java
+++ src/new main/Integrated/src/new_simvasos/scenario/Action.java
@@ -3,11 +3,23 @@ package new_simvasos.scenario;
 public class Action {
 
   String name;
+
+  /**
+   * Instantiates a new Action.
+   *
+   * @param name the name
+   */
   public Action(String name) {  // Constructor
     this.name = name;
   }
 
-  // Change "do" function in the prototype into "bahave"
+  /**
+   * A method to occur an action.
+   * This method should be inherited and re-defined by user.
+   *
+   * @return the string
+   */
+// Change "do" function in the prototype into "bahave"
   // Because there is already default "do" function in Java
   public String behave() {
     return "do action: " + this.name;
