4f758efb69d80cfd1cf788898f59509e71da8012
zele <zelalem@se.kaist.ac.kr>
2018-12-10 10:39:04 +0900

    propertey check

--- src/Junit_test/property/PropertyCheckerTest.java
+++ src/Junit_test/property/PropertyCheckerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PropertyCheckerTest {
-
+// checking the property of constituent systems
     @Test
     public void evaluateState() {
     }
