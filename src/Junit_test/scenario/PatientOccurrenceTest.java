ff0313248201e7ef81d1198bcbafc8c2e53430e6
zele <zelalem@se.kaist.ac.kr>
2018-12-10 10:36:26 +0900

    Patient test

--- src/Junit_test/scenario/PatientOccurrenceTest.java
+++ src/Junit_test/scenario/PatientOccurrenceTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PatientOccurrenceTest {
-
+// test to check patient occurance 
     @Test
     public void behave() {
     }
--- src/Junit_test/scenario/ScenarioTest.java
+++ src/Junit_test/scenario/ScenarioTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class ScenarioTest {
 //scenario test
-//define variables and parameters that need to be tested 
+//define variables and parameters that need to be tested
     @Test
     public void reset() {
     }
