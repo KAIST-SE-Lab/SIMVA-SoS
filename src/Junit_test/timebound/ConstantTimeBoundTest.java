0f98f8dc02216b84cd997f484dec7661e23c0fdb
zele <zelalem@se.kaist.ac.kr>
2018-12-10 10:33:24 +0900

    Update ConstantTimeBoundTest.java

--- src/Junit_test/timebound/ConstantTimeBoundTest.java
+++ src/Junit_test/timebound/ConstantTimeBoundTest.java
@@ -4,7 +4,7 @@ import new_simvasos.timebound.TimeBound;
 import org.junit.Test;
 
 public class ConstantTimeBoundTest extends TimeBound {
-//     
+//     constraints 
     @Test
     public int getTick() {
         return 0;
