62304f96d02bd95db72a728828c8f9dfd6c2fa6b
abalon1210 <abalon1210@daum.net>
2018-12-12 13:07:10 +0900

    Mutation Testing Configuration - MCIPropertyChecker

--- src/new main/Integrated/src/new_simvasos/property/MCIPropertyChecker.java
+++ src/new main/Integrated/src/new_simvasos/property/MCIPropertyChecker.java
@@ -9,43 +9,47 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class MCIPropertyChecker extends ExistenceChecker {
-
-  public MCIPropertyChecker() {
-
-    super();
-  }
-  
-  @Override
-  protected boolean evaluateState(Snapshot snapshot, Property verificationProperty) {
-    StringTokenizer st = new StringTokenizer(snapshot.getSnapshotString(), " ");
-    while(st.hasMoreTokens()) {
-      if(st.nextToken().equals("RescuedRate:"))
-        break;
+    
+    public MCIPropertyChecker() {
+        
+        super();
     }
-
-
-    double rescueRate = Double.parseDouble(st.nextToken());
-
-    if(rescueRate > verificationProperty.getValue()){
-      return true;
+    
+    @Override
+    protected boolean evaluateState(Snapshot snapshot, Property verificationProperty) {
+        StringTokenizer st = new StringTokenizer(snapshot.getSnapshotString(), " ");
+        while(st.hasMoreTokens()) {
+            if(st.nextToken().equals("RescuedRate:"))
+                break;
+        }
+        
+        double rescueRate = Double.parseDouble(st.nextToken());
+        
+        if(rescueRate > verificationProperty.getValue()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-      return false;
+    
+    @Override
+    public boolean check(Log log, Property verificationProperty, int until) {
+        return false;
     }
-  }
-
-  @Override
-  public boolean check(Log log, Property verificationProperty, int until) {
-    return false;
-  }
-
-  @Override
-  public boolean check(Log log, Property verificationProperty, double prob, int T) {
-    return false;
-  }
-
-  @Override
-  public boolean check(Log log, Property verificationProperty, double prob, int t, int T) {
-    return false;
-  }
+    
+    @Override
+    public boolean check(Log log, Property verificationProperty, double prob, int T) {
+        return false;
+    }
+    
+    @Override
+    public boolean check(Log log, Property verificationProperty, double prob, int t, int T) {
+        return false;
+    }
+    
+    @Override
+    public boolean check(Log log, Property verificationProperty, int t, int T) {
+        return false;
+    }
 }
--- src/new main/Integrated/src/new_simvasos/verifier/SPRT.java
+++ src/new main/Integrated/src/new_simvasos/verifier/SPRT.java
@@ -8,98 +8,198 @@ import new_simvasos.simulation.Simulation;
 import new_simvasos.simulator.Simulator;
 
 public class SPRT extends Verifier {
-  double alpha;
-  double beta;
-  double delta;
-  int minimumSample;
-  
-  public SPRT(PropertyChecker checker) {
-    super(checker);
-    this.alpha = 0.05;
-    this.beta = 0.05;
-    this.delta = 0.01;
-    this.minimumSample = 2;
-  }
-  
+    double alpha;
+    double beta;
+    double delta;
+    int minimumSample;
+    
+    public SPRT(PropertyChecker checker) {
+        super(checker);
+        this.alpha = 0.05;
+        this.beta = 0.05;
+        this.delta = 0.01;
+        this.minimumSample = 2;
+    }
+    
     // Return <pair for drawing graph<number of samples, true/false>,
     // verification result on each theta>
-  public Pair<Pair<Integer,Boolean>, String> verifyWithSimulationGUI(Simulation simulation, Property verificationProperty, int maxRepeat, double theta) {
+    // Existence, Absence, Universality Check
+    public Pair<Pair<Integer,Boolean>, String> verifyWithSimulationGUI(Simulation simulation, Property verificationProperty, int maxRepeat, double theta) {
         int maxNumSamples = maxRepeat;
         boolean ret = true;
         int numSamples;
         int numTrue;
-
+        
         numSamples = 0;
         numTrue = 0;
-
-        while(this.isSampleNeeded(numSamples, numTrue, theta)) {
+        
+        while (this.isSampleNeeded(numSamples, numTrue, theta)) {
             if (!(numSamples < maxNumSamples)) {
                 System.out.println("Over maximum repeat: " + maxNumSamples);
                 break;
             }
-
+            
             Log log = simulation.runSimulation();
             
-            if(this.propertychecker.check(log, verificationProperty)) {
+            if (this.propertychecker.check(log, verificationProperty)) {
+                
                 numTrue += 1;
             }
             numSamples += 1;
         }
-
+        
         ret = this.isSatisfied(numSamples, numTrue, theta);
-        String verificationResult = "theta: " + Double.parseDouble(String.format("%.2f",theta)) +
-                " numSamples: " + numSamples + " numTrue: " + numTrue + " result: " + ret;
-
-
+        String verificationResult = "theta: " + Double.parseDouble(String.format("%.2f", theta)) +
+            " numSamples: " + numSamples + " numTrue: " + numTrue + " result: " + ret;
+        
+        
         return new Pair(new Pair(numSamples, ret), verificationResult);
     }
-  
-  private boolean isSampleNeeded(int numSample, int numTrue, double theta) {
-    if (numSample < this.minimumSample) return true;
-    
-    double h0Threshold = this.beta / (1-this.alpha);
-    double h1Threshold = (1-this.beta) / this.alpha;
-    
-    double v = this.getV(numSample, numTrue, theta);
-    
-    if (v <= h0Threshold) {
-      return false;
-    }
-    else if (v >= h1Threshold) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-  
-  private boolean isSatisfied(int numSamples, int numTrue, double theta) {
-    double h0Threshold = this.beta/(1-this.alpha);
-    
-    double v = this.getV(numSamples, numTrue, theta);
-    
-    if (v <= h0Threshold) {
-      return true;
-    } else  {
-      return false;
-    }
-  }
-  
-  private double getV(int numSample, int numTrue, double theta) {
-    double p0 = theta + this.delta;
-    double p1 = theta - this.delta;
-    
-    int numFalse = numSample - numTrue;
-    
-    double p1m = Math.pow(p1, numTrue) * Math.pow((1-p1), numFalse);
-    double p0m = Math.pow(p0, numTrue) * Math.pow((1-p0), numFalse);
-    
-    if (p0m == 0) {
-      p1m = p1m + Double.MIN_VALUE;
-      p0m = p0m + Double.MIN_VALUE;
+    // Minimum Duration Check
+    public Pair<Pair<Integer,Boolean>, String> verifyWithSimulationGUI(Simulation simulation, Property verificationProperty, int maxRepeat, double theta, int t, int T) {
+        int maxNumSamples = maxRepeat;
+        boolean ret = true;
+        int numSamples;
+        int numTrue;
+        
+        numSamples = 0;
+        numTrue = 0;
+        
+        while (this.isSampleNeeded(numSamples, numTrue, theta)) {
+            if (!(numSamples < maxNumSamples)) {
+                System.out.println("Over maximum repeat: " + maxNumSamples);
+                break;
+            }
+            
+            Log log = simulation.runSimulation();
+            
+            //MinimumDurationChecker
+            if(this.propertychecker.check(log, verificationProperty, t, T)) {
+                numTrue += 1;
+            }
+            numSamples += 1;
+        }
+        
+        ret = this.isSatisfied(numSamples, numTrue, theta);
+        String verificationResult = "theta: " + Double.parseDouble(String.format("%.2f", theta)) +
+            " numSamples: " + numSamples + " numTrue: " + numTrue + " result: " + ret;
+        
+        
+        return new Pair(new Pair(numSamples, ret), verificationResult);
     }
     
-    return p1m / p0m;
-  }
-  
+    // SteadyStateProbability Check
+    public Pair<Pair<Integer,Boolean>, String> verifyWithSimulationGUI(Simulation simulation, Property verificationProperty, int maxRepeat, double theta, double p, int T) {
+        int maxNumSamples = maxRepeat;
+        boolean ret = true;
+        int numSamples;
+        int numTrue;
+        
+        numSamples = 0;
+        numTrue = 0;
+        
+        while (this.isSampleNeeded(numSamples, numTrue, theta)) {
+            if (!(numSamples < maxNumSamples)) {
+                System.out.println("Over maximum repeat: " + maxNumSamples);
+                break;
+            }
+            
+            Log log = simulation.runSimulation();
+            
+            if(this.propertychecker.check(log, verificationProperty, p, T)) {
+                numTrue += 1;
+            }
+            numSamples += 1;
+        }
+        
+        ret = this.isSatisfied(numSamples, numTrue, theta);
+        String verificationResult = "theta: " + Double.parseDouble(String.format("%.2f", theta)) +
+            " numSamples: " + numSamples + " numTrue: " + numTrue + " result: " + ret;
+        
+        
+        return new Pair(new Pair(numSamples, ret), verificationResult);
+    }
+    
+    // Transient State Probability Check
+    public Pair<Pair<Integer,Boolean>, String> verifyWithSimulationGUI(Simulation simulation, Property verificationProperty, int maxRepeat, double theta, double p, int t, int T) {
+        int maxNumSamples = maxRepeat;
+        boolean ret = true;
+        int numSamples;
+        int numTrue;
+        
+        numSamples = 0;
+        numTrue = 0;
+        
+        while (this.isSampleNeeded(numSamples, numTrue, theta)) {
+            if (!(numSamples < maxNumSamples)) {
+                System.out.println("Over maximum repeat: " + maxNumSamples);
+                break;
+            }
+            
+            Log log = simulation.runSimulation();
+            
+            if(this.propertychecker.check(log, verificationProperty, p, t, T)) {
+                numTrue += 1;
+            }
+            numSamples += 1;
+        }
+        
+        ret = this.isSatisfied(numSamples, numTrue, theta);
+        String verificationResult = "theta: " + Double.parseDouble(String.format("%.2f", theta)) +
+            " numSamples: " + numSamples + " numTrue: " + numTrue + " result: " + ret;
+        
+        
+        return new Pair(new Pair(numSamples, ret), verificationResult);
+    }
+    
+    private boolean isSampleNeeded(int numSample, int numTrue, double theta) {
+        if (numSample < this.minimumSample) return true;
+        
+        double h0Threshold = this.beta / (1-this.alpha);
+        double h1Threshold = (1-this.beta) / this.alpha;
+        
+        double v = this.getV(numSample, numTrue, theta);
+        
+        if (v <= h0Threshold) {
+            return false;
+        }
+        else if (v >= h1Threshold) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    private boolean isSatisfied(int numSamples, int numTrue, double theta) {
+        double h0Threshold = this.beta/(1-this.alpha);
+        
+        double v = this.getV(numSamples, numTrue, theta);
+        
+        if (v <= h0Threshold) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+    
+    private double getV(int numSample, int numTrue, double theta) {
+        double p0 = theta + this.delta;
+        double p1 = theta - this.delta;
+        
+        int numFalse = numSample - numTrue;
+        
+        double p1m = Math.pow(p1, numTrue) * Math.pow((1-p1), numFalse);
+        double p0m = Math.pow(p0, numTrue) * Math.pow((1-p0), numFalse);
+        
+        if (p0m == 0) {
+            p1m = p1m + Double.MIN_VALUE;
+            p0m = p0m + Double.MIN_VALUE;
+        }
+        
+        return p1m / p0m;
+    }
+    
 }
+
+
