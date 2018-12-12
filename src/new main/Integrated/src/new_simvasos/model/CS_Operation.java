16d49dae0bc13bc64c8473c85cc7037050b19de8
ymbaek <ymbaek@se.kaist.ac.kr>
2018-12-12 20:25:22 +0900

    (Emergency Commit)18

--- src/new main/Integrated/src/new_simvasos/model/CS_Operation.java
+++ src/new main/Integrated/src/new_simvasos/model/CS_Operation.java
@@ -9,7 +9,8 @@ import new_simvasos.model.Abstract.SIMVASoS_Operation;
  * Thus, multiple CS_Operation can be included in a single SIMVASoS_Action (CS_Action).
  * Definition of CS_Operations is not mandatory, but it enables more systematic action definition.
  *
- * SIMVASoS_Operation has its operationType and abstract method operate()
+ * To make a concrete CS_Operation object,
+ * an abstract method operate() from SIMVASoS_Operation should be implemented.
  */
 public abstract class CS_Operation extends SIMVASoS_Operation {
     String csBehaviorLog;
@@ -20,4 +21,14 @@ public abstract class CS_Operation extends SIMVASoS_Operation {
     }
 
     //TODO: Find usage of CS_Operation (also, SIMVASoS_Operation) and its attributes/operations
+
+    /* GETTERS & SETTERS */
+
+    public String getCsBehaviorLog() {
+        return csBehaviorLog;
+    }
+
+    public void setCsBehaviorLog(String csBehaviorLog) {
+        this.csBehaviorLog = csBehaviorLog;
+    }
 }
