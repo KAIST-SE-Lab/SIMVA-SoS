8c00c482f675a2fb194b282e96ce16303b59d884
zele <zelalem@se.kaist.ac.kr>
2018-12-13 11:15:04 +0900

    Update CollaborationHandler.java

--- src/main/java/simvasos/policy/CollaborationHandler.java
+++ src/main/java/simvasos/policy/CollaborationHandler.java
@@ -1,6 +1,13 @@
 package main.java.simvasos.policy;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 /* This class is used handle collaboration attributes which includes policy constructs*/
-public class CollaborationHandler {
+public class CollaborationHandler extends DefaultHandler {
+
+    @Override;
+    public void startElement(String uri, String localName, String qName, Attributes att) throws SAXException{
+        
+    }
 }
