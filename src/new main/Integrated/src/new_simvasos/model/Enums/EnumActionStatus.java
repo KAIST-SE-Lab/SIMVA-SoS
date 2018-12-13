c7e8819573ac9a1b3ae1806ee753e71af6efa33c
ymbaek <ymbaek@se.kaist.ac.kr>
2018-12-13 20:27:31 +0900

    Modify comment14

--- src/new main/Integrated/src/new_simvasos/model/Enums/EnumActionStatus.java
+++ src/new main/Integrated/src/new_simvasos/model/Enums/EnumActionStatus.java
@@ -2,7 +2,7 @@ package new_simvasos.model.Enums;
 
 public enum EnumActionStatus {
     UNAVAILABLE,    //An action is unavailable
-    NOT_STARTED,
+    NOT_STARTED,    //An action is not yet started
     EXECUTING,
     PAUSED,
     PENDED,
