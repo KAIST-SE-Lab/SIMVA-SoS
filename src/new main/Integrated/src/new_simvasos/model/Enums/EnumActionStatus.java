c4d45c4b17d986d7442a10357731c4b195cb0561
ymbaek <ymbaek@se.kaist.ac.kr>
2018-12-13 20:30:01 +0900

    Modify comment20

--- src/new main/Integrated/src/new_simvasos/model/Enums/EnumActionStatus.java
+++ src/new main/Integrated/src/new_simvasos/model/Enums/EnumActionStatus.java
@@ -2,6 +2,8 @@ package new_simvasos.model.Enums;
 
 /**
  * Enumeration for describing status of an action
+ *
+ * According to the action status, an action either can be or can't be selected by a CS.
  */
 public enum EnumActionStatus {
     UNAVAILABLE,    //An action is unavailable
