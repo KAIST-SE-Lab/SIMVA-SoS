402795ea0e14031015c917b380bc8c8bc230141d
zele <zelalem@se.kaist.ac.kr>
2018-12-13 11:22:12 +0900

    Add

--- src/main/java/simvasos/policy/GoalHandler.java
+++ src/main/java/simvasos/policy/GoalHandler.java
@@ -1,6 +1,10 @@
 package main.java.simvasos.policy;
 
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class GoalHandler extends DefaultHandler {
+    public void characters(char ch[], int start, int length) throws SAXException {
+
+    }
 }
