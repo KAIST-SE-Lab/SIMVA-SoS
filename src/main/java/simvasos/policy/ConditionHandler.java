37ac5096ccb1105413d4c3a50c515c24095e8707
zele <zelalem@se.kaist.ac.kr>
2018-12-13 11:19:42 +0900

    Update ConditionHandler.java

--- src/main/java/simvasos/policy/ConditionHandler.java
+++ src/main/java/simvasos/policy/ConditionHandler.java
@@ -1,4 +1,12 @@
 package main.java.simvasos.policy;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 /* This class is used to handle condition elements including envelop actions*/
-public class ConditionHandler {
+public class ConditionHandler extends DefaultHandler {
+    public void startElement(String uri, String localName, String qName, Attributes att) throws SAXException {
+
+    }
 }
