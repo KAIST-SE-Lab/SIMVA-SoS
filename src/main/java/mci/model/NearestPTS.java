baade706a510a2b1bbcf5bcd21678a330560a2b9
Zele <34395169+zele-git@users.noreply.github.com>
2020-11-17 19:23:06 +0900

    Update NearestPTS.java

--- src/main/java/mci/model/NearestPTS.java
+++ src/main/java/mci/model/NearestPTS.java
@@ -27,7 +27,7 @@ public class NearestPTS extends BasePTS{
         int patientSavingBenefit = 80;
         if(rA.isAcknowledged())
             patientSavingBenefit += 20;
-
+//description about the static integer values 
         int distance = rA.getRaisedLoc() > 50? rA.getRaisedLoc()-50 : 50 - rA.getRaisedLoc();
         distance *= 2;
 
